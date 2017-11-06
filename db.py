ccc7781bce723397140d086cef9879f391575129
Sylvain <girod_s@epitech.eu>
2017-11-06 22:27:29 +0100

    Works better with python functions names

--- db.py
+++ db.py
@@ -15,5 +15,5 @@ class Database:
         r.connect(self.db_address, self.db_port).repl()
 
     def create_db(self):
-        if not r.dblist().contains(self.db_name):
+        if not r.db_list().contains(self.db_name):
             r.db_create(self.db_name).run()
