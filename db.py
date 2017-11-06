68779b22ff6939615cc7faf522f9d526e099061c
Sylvain <girod_s@epitech.eu>
2017-11-06 22:14:19 +0100

    fixing db object and removing create_db() parameters

--- db.py
+++ db.py
@@ -6,14 +6,14 @@ class Database:
     db_port = 0
     db_name = ""
 
-    def __init__(db_address = "localhost", db_port = 28015, db_name = "eniguima"):
+    def __init__(self, db_address = "localhost", db_port = 28015, db_name = "eniguima"):
         self.db_address = db_address
         self.db_port = db_port
         self.db_name = db_name
 
-    def connect():
+    def connect(self):
         r.connect(self.db_address, self.db_port).repl()
 
-    def create_db(db_name = self.db_name):
-        if not r-dblist().contains(db_name):
-            r.db_create(db_name).run()
+    def create_db(self):
+        if not r-dblist().contains(self.db_name):
+            r.db_create(self.db_name).run()
